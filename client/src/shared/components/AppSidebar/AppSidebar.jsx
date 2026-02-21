5bf62ba991a1e5058e37c1b51a148800ea51e86a
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-02-21 10:39:03 +0700

    feat(ui): add AppSidebar component
    This is the main sidebar of the application, which is used in the
    MainLayout. I removed the test file since for now the component is not
    worth of testing because it has no logic apart from expand/collapse.

--- client/src/shared/components/AppSidebar/AppSidebar.jsx
+++ client/src/shared/components/AppSidebar/AppSidebar.jsx
@@ -1,7 +1,80 @@
 import styles from './AppSidebar.module.scss';
+import { Link } from 'react-router';
+import { useState } from 'react';
+import { AppLogo, ProfileMenu } from '..';
 
 const AppSidebar = () => {
-  return <aside className={styles.appSidebar}>App Sidebar</aside>;
+  const sidebarItems = [
+    {
+      path: '/conversations',
+      label: 'Inbox',
+      iconRegular: 'fi fi-rr-messages',
+      iconSolid: 'fi fi-sr-messages',
+    },
+    {
+      path: '/people',
+      label: 'People',
+      iconRegular: 'fi fi-rr-users',
+      iconSolid: 'fi fi-sr-users',
+      badge: '9',
+    },
+    {
+      path: '/settings',
+      label: 'Settings',
+      iconRegular: 'fi fi-rr-settings-sliders',
+      iconSolid: 'fi fi-br-settings-sliders',
+    },
+  ];
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <aside
+      className={`${styles.appSidebar} ${expanded ? styles.expanded : ''}`}
+      aria-expanded={expanded}
+    >
+      <header>
+        <Link to="/" aria-label="Go to homepage" className={styles.appLogo}>
+          <AppLogo />
+        </Link>
+        <button
+          className={`${styles.sidebarAction} ${styles.expandButton}`}
+          aria-label={expanded ? 'Collapse sidebar' : 'Expand sidebar'}
+          onClick={() => setExpanded((prev) => !prev)}
+        >
+          <i
+            className={`fi fi-rr-angle-double-small-right ${styles.expandIcon}`}
+          ></i>
+          <i
+            className={`fi fi-rr-angle-double-small-left ${styles.collapseIcon}`}
+          ></i>
+        </button>
+      </header>
+      <nav className={styles.navigation}>
+        <ul>
+          {sidebarItems.map((item) => (
+            <li key={item.label}>
+              <Link
+                className={`${styles.sidebarAction} ${location.pathname === item.path ? styles.active : ''}`}
+                to={item.path}
+              >
+                <i className={`${item.iconRegular} ${styles.regularIcon}`}></i>
+                <i className={`${item.iconSolid} ${styles.solidIcon}`}></i>
+                <span className={styles.label}>{item.label}</span>
+                {item.badge && (
+                  <span className={styles.badge}>
+                    <span className={styles.badgeText}>{item.badge}</span>
+                  </span>
+                )}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <footer>
+        <ProfileMenu expanded={expanded} />
+      </footer>
+    </aside>
+  );
 };
 
 export default AppSidebar;
